75bf7ca324b6b0efc449a8978b03a29ecd18d1bb
Rebecca Jackson <zimbecs@gmail.com>
2019-01-25 12:28:48 +1300

    rerouting

--- routes/routes.js
+++ routes/routes.js
@@ -5,40 +5,28 @@ const db = require('../db')
 const router = express.Router()
 
 router.get('/',(req, res) => {
-  
   db.showIng()
   .then(ing =>{
-    console.log({ing})
     res.render('home', {ing})
   })
 })
 
-router.post('/search-ing', (req, res) => {
+
+router.get('/home/:id', (req, res) => {
   var id = req.body.id
-  var name = req.body.name
-  db.findIng(id, name)
-  .then((result) => {
-    res.redirect('/option', result)
-  })
-   .catch(err => {
-     res.status(500).send('error')
-   })
+  db.viewDish(id)
+    .then((result) => {
+      res.render('option', result)
+    })
+    .catch(err => {
+      res.status(500).send('error')
+    })
 })
 
-router.get('/option', (req, res) => {
-  res.render('option')
-})
+// router.get('', (req,res) => {
 
-router.post('/option/:id', (req,res) => {
-   var recipe = req.body
-   db.addIng(recipe)
-     .then((result) => {
-       res.render('/view')
-     })
-     .catch(err => {
-       res.status(500).send('error')
-     })
-})
+  
+// })
 
 router.get('/view/:id', (req,res) => {
   //needs id from req.body
