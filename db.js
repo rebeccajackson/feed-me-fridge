26f5475f28ac635ad5bb2f6394d82cbb84102634
Sam-Bartos <sambartos@gmail.com>
2019-01-25 10:08:16 +1300

    add get ingredient fn in db

--- db.js
+++ db.js
@@ -2,7 +2,12 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
+function getIng (db = connection){
+    return db('ing').select()
+}
+
 module.exports = {
+    getIng: getIng
 // export the functions
 }
 // rename functions and make them work for our fridge
