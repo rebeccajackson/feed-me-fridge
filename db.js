7f88c42785d25f41865a0425435bce6ff1d3d000
mijucho <chomiju38@gmail.com>
2019-01-25 10:27:24 +1300

    modified routes

--- db.js
+++ db.js
@@ -3,9 +3,7 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  findIng,
-  showDishes,
-  showRecipe
+// export the functions
 }
 // rename functions and make them work for our fridge
 // function getUsers (db = connection) {
@@ -15,19 +13,3 @@ module.exports = {
 // function getUser (id, db = connection) {
 //   return db('users').where('id', id).first()
 // }
-
-
-function findIng(
-
-  //get all recipes where ing match recipe
-  //need .filter
-  // return array of dishes
-){}
-
-function showDishes(name, db = connection) {
-  return db('ing_dish')
-}
-
-function showRecipe(id, db = connection) {
-  
-}
